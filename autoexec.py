c63c531e5193e92675fd4f93fe1c72fad2417765
Chris <chris@Hammer.local>
2014-12-20 11:05:25 +0800

    optional autoexec.py

---
+++ autoexec.py
@@ -0,0 +1,4 @@
+# put this into your userdata folder to start HKTV App at XBMC startup
+# check out http://kodi.wiki/view/userdata for location of userdata folder in your device
+import xbmc
+xbmc.executebuiltin('XBMC.RunAddon(plugin.video.hktv-app)')
